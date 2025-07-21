4ed484a41d6340b1d547d898bef6e7fec49fb96f
Tarae0419 <tarae0419@gmail.com>
2025-07-21 15:28:09 +0900

    refactor: 금리 타입 String -> double

--- src/main/java/org/scoula/finance/domain/deposit.java
+++ src/main/java/org/scoula/finance/domain/deposit.java
@@ -8,8 +8,8 @@ public class deposit {
     private String depositProductName; // 상품명
     private String depositContractPeriod; // 계약기간
     private String depositSubscriptionAmount; // 가입금액
-    private String depositBasicRate; // 기본 금리
-    private String depositMaxRate; // 최고 금리
+    private double depositBasicRate; // 기본 금리
+    private double depositMaxRate; // 최고 금리
     private String depositPreferentialRate; //우대 이율
     private String depositProductFeatures; // 상품 특징
     private String depositSummary; // 상품 요약
