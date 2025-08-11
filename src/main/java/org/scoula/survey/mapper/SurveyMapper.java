168e5f232a5fc28c9ba39f80a30352f6c69159ab
lego812 <suwontaran@hs.ac.kr>
2025-08-11 21:33:29 +0900

    feat: 투자성향기능 추가
    투자성향설문을 5문항버전과 10문항 버전으로 분리
    설문완료여부 조회 엔드포인트 추가
    투자성향설문 완료 시, 결과에 대한 설명 추가
    
    closes #107

--- src/main/java/org/scoula/survey/mapper/SurveyMapper.java
+++ src/main/java/org/scoula/survey/mapper/SurveyMapper.java
@@ -1,12 +1,19 @@
 package org.scoula.survey.mapper;
 
 import org.scoula.survey.domain.SurveyVO;
+import org.scoula.survey.dto.SurveyRequestDTO;
 
 public interface SurveyMapper {
+
     //온보딩 시 투자성향결과 삽입
-    void insertSurvey(SurveyVO surveyVO);
+    void insert(SurveyVO surveyVO);
+
+    //투자성향 수정
+    void update(SurveyVO surveyVO);
+
+    //전체설문완료여부 체크
+    Boolean isSurvey(Long userId);
+
     //투자성향 검색
     SurveyVO selectById(Long id);
-    //투자성향 바꾸기-만약 투자성향 수정가능하다면 개발
-    //void updateSurveyResult(InvestmentTypes propensity);
 }
